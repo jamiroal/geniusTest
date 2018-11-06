673c36febaff1f433b18e2d494bfd8d57cd659d0
jamiroal <jamiroal@gmail.com>
2018-11-05 19:22:40 -0500

    Indexation and media queries.

--- functions.js
+++ functions.js
@@ -1,33 +1,19 @@
-function test(a,b,c){
-  x = (b*c)/a;
-}
-
-
-
 function init (lengthOfPeriodInSeconds) {
-    totalTime = lengthOfPeriodInSeconds*10;
-    return totalTime
+  totalTime = lengthOfPeriodInSeconds*10;
+  return totalTime
 }
-
-
+function addMark(perc,mTeaM){
+  var mark = document.createElement("div");
+  mark.setAttribute('class', 'mark');
+  mark.style.left=`${perc}%`;
+  document.getElementById(`${mTeaM}`).appendChild(mark);
+}
 function addAction (timeInSeconds, team) {
-
-  var perc = (100*timeInSeconds)/totalTime;
-
+var perc = (100*timeInSeconds)/totalTime;
   if (team == "HOME") {
-    var mark = document.createElement("div");
-    mark.setAttribute('class', 'mark');
-    mark.style.left=`${perc}%`;
-    document.getElementById('home').appendChild(mark);
-
+    addMark(perc,'home');
   }else if (team == "AWAY") {
-    var mark = document.createElement("div");
-    mark.setAttribute('class', 'mark');
-    mark.style.left=`${perc}%`;
-    document.getElementById('away').appendChild(mark);
+    addMark(perc,'away');
   }
-
-  return console.log("A "+ team + " mark was created in the second " + timeInSeconds + " of "+ totalTime);
-
-
+  console.log("A "+ team + " mark was created in the second " + timeInSeconds + " of "+ totalTime);
 }
